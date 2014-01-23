33c0a3e31595c4b514da70158c5b304aadc0643c
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-01-23 12:49:53 +0100

    Updated FSF's address

--- src/codecfinder.py
+++ src/codecfinder.py
@@ -15,9 +15,7 @@
 # Library General Public License for more details.
 #
 # You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 # THIS CODE CAN PROBABLY BE REDUCED A LOT IN SIZE SINCE ITS 3 BIG FUNCTIONS
 # DOING ESSENTIALLY THE SAME, ESPECIALLY NOW THAT THE ONLY SPECIAL CASING
--- src/transmageddon.py
+++ src/transmageddon.py
@@ -15,9 +15,7 @@
 # Library General Public License for more details.
 #
 # You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 # DEVELOPERS NOTE: If you want to work on this code the two crucial objects are audiodata and videodata. 
 # These two items are lists that contain python dictionaries. They contain almost all important 
